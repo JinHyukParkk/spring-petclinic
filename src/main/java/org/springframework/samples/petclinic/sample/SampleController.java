d5bc8fb0903b7d6c7eaa2027aa8dcbd244ebe760
hyuk <pjh08190819@naver.com>
2021-08-06 23:57:43 +0900

    add aspect

--- src/main/java/org/springframework/samples/petclinic/sample/SampleController.java
+++ src/main/java/org/springframework/samples/petclinic/sample/SampleController.java
@@ -2,6 +2,7 @@ package org.springframework.samples.petclinic.sample;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.samples.petclinic.aspect.LogExecutionTime;
 import org.springframework.samples.petclinic.owner.OwnerRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ public class SampleController {
 	@Autowired
 	String hyuk;
 
+	@LogExecutionTime
 	@GetMapping("/context")
 	public String context() {
 		return "hello " + applicationContext.getBean(OwnerRepository.class);
